f163af5231efd591083313cd2aac2482e475287a
rumeysaexe <rumeysa.main@gmail.com>
2024-11-16 22:36:08 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int satirSayisi = 6; 
+            for (int i = 1; i <= satirSayisi; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*"); 
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
